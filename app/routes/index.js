63253ee2742a13604acc2dba2c417c2986761f28
ming-soon <ming.soon413@gmail.com>
2017-02-06 15:27:40 +0800

    Integrate redux and router.

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, IndexRoute } from 'react-router'
+import CoreLayout from '../layouts/CoreLayout'
+import Home from './Home'
+
+export default (
+  <Route path="/" component={CoreLayout}>
+    <IndexRoute component={Home} />
+  </Route>
+)
