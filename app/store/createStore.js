bd2040fd6dfabadb9af5dbebc1025e813c7aff07
ming-soon <ming.soon413@gmail.com>
2017-02-09 17:49:33 +0800

    Apply thunk middleware.

--- app/store/createStore.js
+++ app/store/createStore.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import makeRootReducer from './reducers'
 
 export default (initialState = {}) => {
   const store = createStore(
     makeRootReducer(),
-    initialState // eslint-disable-line comma-dangle
+    initialState,
+    applyMiddleware(thunk) // eslint-disable-line comma-dangle
   )
 
   if (module.hot) {
